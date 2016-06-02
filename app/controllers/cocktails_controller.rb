f0f40a94f0e01614954b4153b4d4420570786aa2
Jurjen Munneke <jurjenmunneke@me.com>
2016-06-02 15:21:27 +0200

    Added the controllers

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,37 @@
 class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(params.require(:cocktail).permit(:name))
+    @cocktail.save!
+    redirect_to cocktails_path(@cocktail)
+  end
+
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(params.require(:cocktail).permit(:name))
+    redirect_to cocktails_path(@cocktail)
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,24 @@
 class DosesController < ApplicationController
+
+  before_action :find_cocktail, only: [ :new, :create ]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = @cocktail.doses.build(dose_params)
+    @dose.save
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
 end
